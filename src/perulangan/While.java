7cb0d40de4a13bdefc05803e29e77f12187665b4
sitihalimatussa'diah <hlmtss18@gmail.com>
2020-04-25 16:48:12 +0700

    Perulangan While

---
+++ src/perulangan/While.java
@@ -0,0 +1,20 @@
+package perulangan;
+
+public class While {
+    public static void main(String[] args) {
+        int isiGelas = 225;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+
+        while (isiGelas != penuh){
+            isiGelas++;
+            System.out.println("Sedang mengisi Gelas....");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }
+
+        System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+}
