1b61b960d5f335a70948d7f7b8093a38e8ebcf6b
sitihalimatussa'diah <hlmtss18@gmail.com>
2020-04-25 20:16:11 +0700

    Tugas Perulangan For

---
+++ src/perulangan/Tugas_For.java
@@ -0,0 +1,12 @@
+package perulangan;
+
+public class Tugas_For {
+    public static void main (String[] args) {
+        System.out.println ("Pengulangan Dengan For Arah Indeks Naik");
+        for (int i = 1; i <= 5; ++i)
+            System.out.println ("Indeks Naik ke = "+i);
+        System.out.println ("Pengulangan Dengan For Arah Indeks Turun");
+        for (int i = 5; i > 0; --i)
+            System.out.println ("Indeks Naik ke = "+i);
+    }
+}
