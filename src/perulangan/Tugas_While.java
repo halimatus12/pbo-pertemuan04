c33828b6f60c8f9bff9bdd1da2eedd13bf416cc8
sitihalimatussa'diah <hlmtss18@gmail.com>
2020-04-25 20:54:18 +0700

    Tugas While

--- src/perulangan/Tugas_While.java
+++ src/perulangan/Tugas_While.java
@@ -1,4 +1,12 @@
 package perulangan;
 
 public class Tugas_While {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i <10) {
+            System.out.println(i);
+            i++;
+        }
+    }
 }
